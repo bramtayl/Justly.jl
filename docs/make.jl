7dab21a89818691ca432829976791163033cd710
Brandon Taylor <brandon.taylor221@gmail.com>
2021-09-01 21:46:38 -0400

    update

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,12 @@
 using Justly
 using Documenter: deploydocs, makedocs
 
-makedocs(sitename = "Justly.jl", modules = [Justly], doctest = false)
+makedocs(
+    sitename = "Justly.jl", 
+    modules = [Justly], 
+    doctest = false,
+    pages = [
+        "Public interface" => "index.md"
+    ]
+)
 deploydocs(repo = "github.com/bramtayl/Justly.jl.git")
